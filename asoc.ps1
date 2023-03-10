f560607d2394afbf810a528f6282f5dc4cf21274
Antony Chiu <chiu_antony@hcl.com>
2023-03-10 19:03:16 +0800

    v1.3

--- asoc.ps1
+++ asoc.ps1
@@ -2,44 +2,94 @@ Write-Host "Starting ASoC script"
 
 #DEBUG
 Write-Warning "Print environment variables:"
-Write-Host "inputs:application_id: " $env:INPUT_APPLICATION_ID
-Write-Host "inputs:baseurl: " $env:INPUT_baseurl
-Write-Host $input:INPUT_baseurl
-Write-Host ${INPUT_baseurl}
-Write-Host ${$input:INPUT_baseurl}
-Write-Host ${$input:BASEURL}
-Write-Host $input:baseurl
-Write-Host ${inputs.application_id}
-Write-Host ${inputs.baseurl}
-
-
-Write-Host ${github.action_path}
 Write-Host "github.sha: " $env:GITHUB_SHA
 dir env:
 
-# ASoC - Login to ASoC with API Key and Secret
-$jsonBody = "
-{
-`"KeyId`": `"$env:INPUT_asoc_key`",
-`"KeySecret`": `"$env:INPUT_asoc_secret`"
-}
-"
-
-$params = @{
-    Uri         = "$env:INPUT_baseurl/Account/ApiKeyLogin"
-    Method      = 'POST'
-    Body        = $jsonBody
-    Headers = @{
-        'Content-Type' = 'application/json'
-      }
-    }
-
-Write-Host @params
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
-$Members = Invoke-RestMethod @params
-Write-Host "Auth successful - Token received: $Members.token"
-$bearer_token = $Members.token
-Write-Host $bearer_token
-$ProgressPreference = 'SilentlyContinue'
 
+#VARIABLES
+$global:BearerToken = ""
+
+function ASoC-Login {
+
+
+  $jsonBodyInPSObject = @{
+    KeyId         = $env:INPUT_asoc_key
+    KeySecret     = $env:INPUT_asoc_secret
+  }
+
+  $params = @{
+      Uri         = "$env:INPUT_baseurl/Account/ApiKeyLogin"
+      Method      = 'POST'
+      Body        = $jsonBodyInPSObject | ConvertTo-Json
+      Headers = @{
+          'Content-Type' = 'application/json'
+        }
+      }
+  $Members = Invoke-RestMethod @params
+  #Write-Host "Auth successful - Token received: $Members.token"
+  $global:BearerToken = $Members.token
+}
+
+function Lookup-ASoC-Application ($ApplicationName) {
+
+  $params = @{
+      Uri         = "$env:INPUT_baseurl/Apps/GetAsPage"
+      Method      = 'GET'
+      Headers = @{
+          'Content-Type' = 'application/json'
+          Authorization = "Bearer $global:BearerToken"
+        }
+      }
+  $Members = Invoke-RestMethod @params
+  Write-Host @Members
+  $Members.Items.Contains($ApplicationName)
+
+}
+
+function Run-ASoC-DynamicAnalyzerNoAuth {
+
+
+  $jsonBody = "
+  {
+    `"ScanType`": `"$env:INPUT_scan_type`",
+    `"IncludeVerifiedDomains`": true,
+    `"StartingUrl`": `"$env:INPUT_starting_URL`",
+    `"TestOptimizationLevel`": `"NoOptimization`",
+    `"UseAutomaticTimeout`": true,
+    `"MaxRequestsIn`": 10,
+    `"MaxRequestsTimeFrame`": 1000,
+    `"OnlyFullResults`": true,
+    `"FullyAutomatic`": true,
+    `"ScanName`": `"$env:GITHUB_REPOSITORY $env:GITHUB_SHA`",
+    `"EnableMailNotification`": true,
+    `"Locale`": `"en-US`",
+    `"AppId`": `"$env:INPUT_application_id`",
+    `"Execute`": true,
+    `"Personal`": false,
+  }
+  "
+  $params = @{
+      Uri         = "$env:INPUT_baseurl/Scans/DynamicAnalyzer"
+      Method      = 'POST'
+      Body        = $jsonBody
+      Headers = @{
+          'Content-Type' = 'application/json'
+          Authorization = "Bearer $global:BearerToken"
+        }
+      }
+  $Members = Invoke-RestMethod @params
+  write-host $Members
+}
+
+#MAIN
+ASoC-Login
+
+if($global:BearerToken -ne ""){
+  Write-Host "Login successful"
+}else{
+  Write-Host "Login failed, bearer token is empty... exiting"
+  exit 1
+}
+
+Run-ASoC-DynamicAnalyzerNoAuth
--- testMain.ps1
+++ testMain.ps1
@@ -1,5 +1,18 @@
 $env:INPUT_baseurl = "https://cloud.appscan.com/api/V2"
 $env:INPUT_asoc_key = "082c4037-2bd5-380a-09e1-a1754a5eaab0"
 $env:INPUT_asoc_secret = "fMM4inw4zR2ip3ovtZHyVLoRenA2sd9Fq9MRNhjTx5s="
-$env:INPUT_application_id = 'appid'
+$env:INPUT_application_id = 'acd3ef50-6276-461d-8514-abc6e7113577'
+$env:INPUT_scan_name = 'sample scan name'
+$env:GITHUB_SHA = "sha265-random-sha-value"
+$env:GITHUB_REPOSITORY = "antonychiu2/github-demo"
+$env:INPUT_scan_type = 'staging' #production, staging
+
+$env:INPUT_login_method = 'none' #none, userpass, record
+$env:INPUT_starting_URL = "https://demo.testfire.net?mode=demo"
+
+#IF LOGIN = NONE
+
+
+$env:INPUT_application_name = 'Github Action Demo Application'
+
 ./asoc.ps1
