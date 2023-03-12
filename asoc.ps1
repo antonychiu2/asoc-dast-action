90436f680b768db0798859c602cb3d9645e5b0f8
Antony Chiu <chiu_antony@hcl.com>
2023-03-12 21:18:11 +0800

    v1.9

--- asoc.ps1
+++ asoc.ps1
@@ -5,13 +5,38 @@ Write-Warning "Print environment variables:"
 Write-Host "github.sha: " $env:GITHUB_SHA
 dir env:
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 
 #VARIABLES
 $global:BearerToken = ""
+$global:scan_name = "$env:GITHUB_REPOSITORY $env:GITHUB_SHA"
+$global:jsonBodyInPSObject = ""
+$global:scanId
+$global:BaseAPIUrl = ""
+$global:BaseAPIUrl = $env:INPUT_baseurl + "/api/V2"
 
-function ASoC-Login {
+#INITIALIZE
+#Construct base JSON Body for DAST Scan for API DynamicAnalyzer and DynamicAnalyzerWithFiles
+$global:jsonBodyInPSObject = @{
+  ScanType = $env:INPUT_scan_type
+  IncludeVerifiedDomains = $true
+  StartingUrl = $env:INPUT_starting_URL
+  TestOptimizationLevel = $env:INPUT_optimization
+  UseAutomaticTimeout = $true
+  MaxRequestsIn = 10
+  MaxRequestsTimeFrame = 1000
+  OnlyFullResults = $true
+  FullyAutomatic = $true
+  ScanName = $global:scan_name
+  EnableMailNotification = $env:INPUT_email_notification
+  Locale = 'en-US'
+  AppId = $env:INPUT_application_id
+  Execute = $true
+  Personal = $env:INPUT_personal_scan
+}
 
+#FUNCTIONS
+function Login-ASoC {
 
   $jsonBodyInPSObject = @{
     KeyId         = $env:INPUT_asoc_key
@@ -19,7 +44,7 @@ function ASoC-Login {
   }
 
   $params = @{
-      Uri         = "$env:INPUT_baseurl/Account/ApiKeyLogin"
+      Uri         = "$global:BaseAPIUrl/Account/ApiKeyLogin"
       Method      = 'POST'
       Body        = $jsonBodyInPSObject | ConvertTo-Json
       Headers = @{
@@ -29,6 +54,26 @@ function ASoC-Login {
   $Members = Invoke-RestMethod @params
   #Write-Host "Auth successful - Token received: $Members.token"
   $global:BearerToken = $Members.token
+
+  if($global:BearerToken -ne ""){
+    Write-Host "Login successful"
+  }else{
+    Write-Host "Login failed, bearer token is empty... exiting"
+    exit 1
+  }
+  
+}
+
+function Set-AppScanPresence{
+
+  if($env:INPUT_network -eq 'private'){
+    
+    $global:jsonBodyInPSObject.Add("PresenceId",$env:INPUT_presence_id)
+<# 
+    $global:jsonBodyInPSObject =+ @{
+      PresenceId = $env:INPUT_presence_id
+    } #> 
+  }
 }
 
 function Lookup-ASoC-Application ($ApplicationName) {
@@ -44,54 +89,235 @@ function Lookup-ASoC-Application ($ApplicationName) {
   $Members = Invoke-RestMethod @params
   Write-Host @Members
   $Members.Items.Contains($ApplicationName)
-
 }
 
-function Run-ASoC-DynamicAnalyzerNoAuth {
 
+function Run-ASoC-FileUpload($filepath){
 
-  $jsonBody = "
-  {
-    `"ScanType`": `"$env:INPUT_scan_type`",
-    `"IncludeVerifiedDomains`": true,
-    `"StartingUrl`": `"$env:INPUT_starting_URL`",
-    `"TestOptimizationLevel`": `"NoOptimization`",
-    `"UseAutomaticTimeout`": true,
-    `"MaxRequestsIn`": 10,
-    `"MaxRequestsTimeFrame`": 1000,
-    `"OnlyFullResults`": true,
-    `"FullyAutomatic`": true,
-    `"ScanName`": `"$env:GITHUB_REPOSITORY $env:GITHUB_SHA`",
-    `"EnableMailNotification`": true,
-    `"Locale`": `"en-US`",
-    `"AppId`": `"$env:INPUT_application_id`",
-    `"Execute`": true,
-    `"Personal`": false,
-  }
-  "
-  write-host $jsonBody
-  
+  #ls -l
+  $uploadedFile = [IO.File]::ReadAllBytes($filepath)
   $params = @{
-      Uri         = "$env:INPUT_baseurl/Scans/DynamicAnalyzer"
-      Method      = 'POST'
-      Body        = $jsonBody
-      Headers = @{
-          'Content-Type' = 'application/json'
-          Authorization = "Bearer $global:BearerToken"
-        }
+    Uri         = "$global:BaseAPIUrl/FileUpload"
+    Method      = 'Post'
+    Headers = @{
+      'Content-Type' = 'multipart/form-data'
+      Authorization = "Bearer $global:BearerToken"
+    }
+     Form = @{
+    'fileToUpload' = Get-Item -Path $filepath
+   }
+  }
+  $upload = Invoke-RestMethod @params
+  $upload_File_ID = $upload.FileId
+  write-host "File Uploaded - File ID: $upload_File_ID"
+
+  return $upload_File_ID
+}
+function Run-ASoC-DynamicAnalyzerNoAuth {
+  Write-Host "Proceeding with no authentications..." -ForegroundColor Green
+
+  return Run-ASoC-DynamicAnalyzerAPI($global:jsonBodyInPSObject | ConvertTo-Json)
+}
+function Run-ASoC-DynamicAnalyzerUserPass{
+  Write-Host "Proceeding with username and password login..." -ForegroundColor Green
+
+  $global:jsonBodyInPSObject.Add("LoginUser",$env:INPUT_login_user)
+  $global:jsonBodyInPSObject.Add("LoginPassword",$env:INPUT_login_password)
+
+  return Run-ASoC-DynamicAnalyzerAPI($jsonBodyInPSObject | ConvertTo-Json)
+}
+
+function Run-ASoC-DynamicAnalyzerRecordedLogin{
+
+  Write-Host "Proceeding with recorded Login..." -ForegroundColor Green
+  #Upload Recorded Login File
+  $FileID = Run-ASoC-FileUpload($env:INPUT_login_sequence_file)
+  $global:jsonBodyInPSObject.Add("LoginSequenceFileId",$FileID)
+  return Run-ASoC-DynamicAnalyzerAPI($jsonBodyInPSObject | ConvertTo-Json)
+}
+
+
+function Run-ASoC-DynamicAnalyzerWithFile{
+
+  $FileID = Run-ASoC-FileUpload($env:INPUT_scan_file)
+  $global:jsonBodyInPSObject.Add("ScanFileId",$FileID)
+
+  return Run-ASoC-DynamicAnalyzerWithFileAPI($jsonBodyInPSObject | ConvertTo-Json)
+}
+
+
+function Run-ASoC-DynamicAnalyzerAPI($json){
+
+  write-host $json
+  $params = @{
+    Uri         = "$global:BaseAPIUrl/Scans/DynamicAnalyzer"
+    Method      = 'POST'
+    Body        = $json
+    Headers = @{
+        'Content-Type' = 'application/json'
+        Authorization = "Bearer $global:BearerToken"
       }
+    }
   $Members = Invoke-RestMethod @params
-  write-host $Members
+  return $Members.Id
+}
+
+function Run-ASoC-DynamicAnalyzerWithFileAPI($json){
+
+  write-host $json
+  $params = @{
+    Uri         = "$global:BaseAPIUrl/Scans/DynamicAnalyzerWithFile"
+    Method      = 'POST'
+    Body        = $json
+    Headers = @{
+        'Content-Type' = 'application/json'
+        Authorization = "Bearer $global:BearerToken"
+      }
+    }
+  $Members = Invoke-RestMethod @params
+  return $Members.Id
+}
+
+function Run-ASoC-DAST{
+
+  #FIRST check if dynamic_scan_type is 'upload' or 'dast'
+  if($env:INPUT_dynamic_scan_type -eq 'upload'){
+    return Run-ASoC-DynamicAnalyzerWithFile
+  
+  #If dynamic_scan_type is not 'upload' then it is a regular 'dast' scan. We proceed to check if it's a userpass login or recorded login
+  }elseif($env:INPUT_login_method -eq 'userpass'){
+    return Run-ASoC-DynamicAnalyzerUserPass
+
+  }elseif($env:INPUT_login_method -eq 'recorded'){
+    return Run-ASoC-DynamicAnalyzerRecordedLogin
+
+  }else{
+    return Run-ASoC-DynamicAnalyzerNoAuth
+  }
+}
+
+function Run-ASoC-ScanCompletionChecker($scanID){
+  $params = @{
+    Uri         = "$global:BaseAPIUrl/Scans/$scanID/Executions"
+    Method      = 'Get'
+    Headers = @{
+      'Content-Type' = 'application/json'
+      Authorization = "Bearer $global:BearerToken"
+    }
+  }
+  $counterTimerInSeconds = 0
+  Write-Host "Waiting for Scan Completion..." -NoNewLine
+  $waitIntervalInSeconds = 30
+
+  while(($scan_status -ne "Ready") -and ($counterTimerInSeconds -lt $env:INPUT_wait_for_analysis_timeout_minutes*60)){
+    $output = Invoke-RestMethod @params
+    $scan_status = $output.Status
+    Start-Sleep -Seconds $waitIntervalInSeconds
+    $counterTimerInSeconds = $counterTimerInSeconds + $waitIntervalInSeconds
+    Write-Host "." -NoNewline
+  }
+  Write-Host ""
+}
+function Run-ASoC-GenerateReport ($scanID) {
+
+  $params = @{
+    Uri         = "$global:BaseAPIUrl/Reports/Security/Scan/$scanID"
+    Method      = 'Post'
+    Headers = @{
+      'Content-Type' = 'application/json'
+      Authorization = "Bearer $global:BearerToken"
+    }
+  }
+  $body = @{
+    'Configuration' = @{
+      'Summary' = "true"
+      'Details' = "true"
+      'Discussion' = "true"
+      'Overview' = "true"
+      'TableOfContent' = "true"
+      'Advisories' = "true"
+      'FixRecommendation' = "true"
+      'History' = "true"
+      'Coverage' = "true"
+      'MinimizeDetails' = "true"
+      'Articles' = "true"
+      'ReportFileType' = "HTML"
+      'Title' = "false"
+      'Locale' = "en-US"
+      'Notes' = "Github SHA: $env:GITHUB_SHA"
+    }
+  }
+  $output_runreport = Invoke-RestMethod @params -Body ($body|ConvertTo-JSON)
+  $report_ID = $output_runreport.Id
+  return $report_ID
+}
+
+function Run-ASoC-ReportCompletionChecker($reportID){
+
+  #Wait for report
+  $params = @{
+    Uri         = "$global:BaseAPIUrl/Reports/$reportID"
+    Method      = 'Get'
+    Headers = @{
+      'Content-Type' = 'application/json'
+      Authorization = "Bearer $global:BearerToken"
+    }
+  }
+  $report_status ="Not Ready"
+  while($report_status -ne "Ready"){
+    $output = Invoke-RestMethod @params
+    $report_status = $output.Status
+    Start-Sleep -Seconds 5
+    Write-Host "Generating Report... Progress: " $output.Progress "%"
+  }
+  
+}
+
+function Run-ASoC-DownloadReport($eportID){
+
+  #Download Report
+  $params = @{
+    Uri         = "$global:BaseAPIUrl/Reports/Download/$eportID"
+    Method      = 'Get'
+    Headers = @{
+      'Accept' = 'text/html'
+      Authorization = "Bearer $global:BearerToken"
+    }
+  }
+  $output_runreport = Invoke-RestMethod @params
+  Out-File -InputObject $output_runreport -FilePath .\AppScan_Security_Report.html
+  
 }
 
 #MAIN
-ASoC-Login
+Login-ASoC
 
-if($global:BearerToken -ne ""){
-  Write-Host "Login successful"
-}else{
-  Write-Host "Login failed, bearer token is empty... exiting"
-  exit 1
+#CHECK NETWORK, if private, then set presence ID
+Set-AppScanPresence
+
+#Run DAST Scan
+$global:scanId = Run-ASoC-DAST
+
+#Display ASoC Scan URL
+$scanOverviewPage = $env:INPUT_baseurl + "/main/myapps/" + $env:INPUT_application_id + "/scans/" + $global:scanId
+Write-Host "Scan is initiated and can be viewed in ASoC Scan Dashboard:" 
+Write-Host $scanOverviewPage -ForegroundColor Green
+
+#If wait_for_analysis is set to true, we proceed to wait before generating report
+if(-not($wait_for_analysis -eq $true)){
+
+  #Check for report completion
+  Run-ASoC-ScanCompletionChecker ($global:scanId)
+
+  #Send for report generation
+  $reportID = Run-ASoC-GenerateReport ($global:scanId)
+  
+  #Check for report generation completion
+  Run-ASoC-ReportCompletionChecker ($reportID)
+
+  #Download report from ASoC
+  Run-ASoC-DownloadReport($reportID){
+
+  }
 }
-
-Run-ASoC-DynamicAnalyzerNoAuth
+ls -l
--- testMain.ps1
+++ testMain.ps1
@@ -1,4 +1,4 @@
-$env:INPUT_baseurl = "https://cloud.appscan.com/api/V2"
+$env:INPUT_baseurl = "https://cloud.appscan.com" 
 $env:INPUT_asoc_key = "082c4037-2bd5-380a-09e1-a1754a5eaab0"
 $env:INPUT_asoc_secret = "fMM4inw4zR2ip3ovtZHyVLoRenA2sd9Fq9MRNhjTx5s="
 $env:INPUT_application_id = 'acd3ef50-6276-461d-8514-abc6e7113577'
@@ -6,13 +6,38 @@ $env:INPUT_scan_name = 'sample scan name'
 $env:GITHUB_SHA = "sha265-random-sha-value"
 $env:GITHUB_REPOSITORY = "antonychiu2/github-demo"
 $env:INPUT_scan_type = 'staging' #production, staging
-
-$env:INPUT_login_method = 'none' #none, userpass, record
 $env:INPUT_starting_URL = "https://demo.testfire.net?mode=demo"
+$env:INPUT_optimization = 'NoOptimization'
+
 
 #IF LOGIN = NONE
 
+#DAST or SCAN File
+$env:INPUT_dynamic_scan_type = "dast"
+$env:INPUT_scan_file = "/Users/antonychiu/Box/HCL/Github Actions/asoc-dast-action/asoc-dast-action/altoro.scant"
 
-$env:INPUT_application_name = 'Github Action Demo Application'
+#NETWORK
+$env:INPUT_network = 'public'
+$env:INPUT_presence_id = 'f185efda-67bf-ed11-ba76-14cb65723612'
+
+#For 'dast' type scan, user will choose the type of login method
+
+$env:INPUT_login_method = 'recorded' #options: none, username, recorded
+
+#IF LOGIN_METHOD = USERPASS
+$env:INPUT_login_user = 'jsmith'
+$env:INPUT_login_password = 'demo1234'
+$env:INPUT_login_extra_field = 'test123'
+
+#IF LOGIN METHOD = RECORD
+$env:INPUT_login_sequence_file = '/Users/antonychiu/Box/HCL/Github Actions/asoc-dast-action/asoc-dast-action/login.dast.config'
+
+#MISC
+$env:INPUT_email_notification = 'true'
+$env:INPUT_personal_scan = 'false'
+$env:INPUT_intervention = 'true'
+$env:INPUT_wait_for_analysis = 'true'
+$env:INPUT_wait_for_analysis_timeout_minutes = '360'
+$env:INPUT_fail_for_noncompliance = 'false'
 
 ./asoc.ps1
