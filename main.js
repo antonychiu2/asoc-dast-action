fbcc05d9663b2b29d961285f6dbd9a7159a1b089
Antony Chiu <chiu_antony@hcl.com>
2023-03-17 01:34:25 +0800

    smalll changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log(process.env)
 var PSFileToRun = "main.ps1";
-var GITHUB_ACTION_PATH = process.env.HOME+"/work/_actions/"+process.env.GITHUB_ACTION_REPOSITORY+"/"+process.env.GITHUB_ACTION_REF;
+process.env.GITHUB_ACTION_PATH = process.env.HOME+"/work/_actions/"+process.env.GITHUB_ACTION_REPOSITORY+"/"+process.env.GITHUB_ACTION_REF;
 console.log('Constructed github action path: '+GITHUB_ACTION_PATH)
 
 var spawn = require("child_process").spawn,child;
