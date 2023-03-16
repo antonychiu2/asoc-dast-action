13e87e8ad00c65e83b8f76b362cd455e572e6e7b
Antony Chiu <chiu_antony@hcl.com>
2023-03-17 01:39:57 +0800

    test

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log(process.env)
 var PSFileToRun = "main.ps1";
-process.env.GITHUB_ACTION_PATH = process.env.HOME+"/work/_actions/"+process.env.GITHUB_ACTION_REPOSITORY+"/"+process.env.GITHUB_ACTION_REF;
+process.env['GITHUB_ACTION_PATH'] = process.env.HOME+"/work/_actions/"+process.env.GITHUB_ACTION_REPOSITORY+"/"+process.env.GITHUB_ACTION_REF;
+
 console.log('Constructed github action path: '+process.env.GITHUB_ACTION_PATH)
 
 var spawn = require("child_process").spawn,child;
