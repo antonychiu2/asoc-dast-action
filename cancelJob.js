154f89fd99b75d720942d20370556d085c17b821
Antony Chiu <chiu_antony@hcl.com>
2023-03-17 01:29:22 +0800

    converted to JS

---
+++ cancelJob.js
@@ -0,0 +1,18 @@
+console.log(process.env)
+var PSFileToRun = "cancelJob.ps1";
+var GITHUB_ACTION_PATH = process.env.HOME+"/work/_actions/"+process.env.GITHUB_ACTION_REPOSITORY+"/"+process.env.GITHUB_ACTION_REF;
+console.log('Constructed github action path: '+GITHUB_ACTION_PATH)
+
+var spawn = require("child_process").spawn,child;
+child = spawn("pwsh",[GITHUB_ACTION_PATH+"/"+PSFileToRun]);
+child.stdout.on("data",function(data){
+    process.stdout.write("" + data);
+});
+child.stderr.on("data",function(data){
+    process.stdout.write("Powershell Errors: " + data);
+    process.exit(1);
+});
+child.on("exit",function(){
+    process.stdout.write("Powershell Script finished");
+});
+child.stdin.end(); //end input
