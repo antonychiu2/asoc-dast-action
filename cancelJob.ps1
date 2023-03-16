96b9cd7d235956a8b7733edb9371a7db91d77739
Antony Chiu <chiu_antony@hcl.com>
2023-03-17 04:22:31 +0800

    test

--- cancelJob.ps1
+++ cancelJob.ps1
@@ -1,13 +1,20 @@
 #During Job Cancellation, we want to also try to stop the scan if it was already initiated
 
+#DEBUG
+dir env:
+$DebugPreference = "Continue"
 
+#INITIALIZE VARIABLES
+$scanidFileName = ".\scanid.txt"
+$global:BaseAPIUrl = $env:INPUT_BASEURL + "/api/V2"
 
 #LOAD ALL ASOC FUNCTIONS FROM LIBRARY FILE asoc.ps1
 . "$env:GITHUB_ACTION_PATH/asoc.ps1"
-$scanidFileName = ".\scanid.txt"
 
-dir env:
+
 $global:scanId = Get-Content $scanidFileName | Select -First 1
 Write-Host "ScanID: $global:scanId"
+
+Login-ASoC
 
 Delete-LatestRunningScanExecution($global:scanId)
